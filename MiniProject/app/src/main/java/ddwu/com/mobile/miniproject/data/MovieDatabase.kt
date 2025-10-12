b830873debb183073901264e0003006193dbf2f7
zuyo7852 <leejuyeon7852@gmail.com>
2025-10-12 13:22:13 +0900

    feat: DB

---
+++ MiniProject/app/src/main/java/ddwu/com/mobile/miniproject/data/MovieDatabase.kt
@@ -0,0 +1,26 @@
+package ddwu.com.mobile.miniproject.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database([MovieEntity::class], version = 1)
+abstract class MovieDatabase : RoomDatabase() {
+    abstract fun movieDao() : MovieDao
+
+    companion object{
+        @Volatile private var INSTANCE: MovieDatabase?=null
+
+        fun getDatabase(context: Context): MovieDatabase{
+            return INSTANCE?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MovieDatabase::class.java, "movie_db"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
