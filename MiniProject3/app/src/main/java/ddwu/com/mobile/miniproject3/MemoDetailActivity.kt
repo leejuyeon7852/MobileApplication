6c9be4ff4c8e04cd1bbd871347d20711c71bf9f9
zuyo7852 <leejuyeon7852@gmail.com>
2025-12-01 12:05:01 +0900

    feat: week14 - 4단계

--- MiniProject3/app/src/main/java/ddwu/com/mobile/miniproject3/MemoDetailActivity.kt
+++ MiniProject3/app/src/main/java/ddwu/com/mobile/miniproject3/MemoDetailActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 //import com.bumptech.glide.Glide
 import ddwu.com.mobile.miniproject3.data.Memo
 import ddwu.com.mobile.miniproject3.databinding.ActivityMemoDetailBinding
@@ -25,7 +26,21 @@ class MemoDetailActivity : AppCompatActivity() {
             insets
         }
 
-//        val memo =
+        val memo = intent.getSerializableExtra("memo") as Memo
+
+        if(memo!=null){
+            binding.etTitle.setText(memo.title)
+            binding.etContents.setText(memo.contents)
+
+            if(memo.imagePath.isNotBlank() && File(memo.imagePath).exists()){
+                Glide.with(this)
+                    .load(File(memo.imagePath))
+                    .into(binding.imageViewPicked)
+            }else{
+                binding.imageViewPicked.setImageResource(android.R.drawable.ic_menu_camera)
+            }
+        }
+
 
 
         binding.imageViewPicked.setOnClickListener {
@@ -33,7 +48,12 @@ class MemoDetailActivity : AppCompatActivity() {
         }
 
         binding.btnUpdate.setOnClickListener {
-
+            val updateMemo = Memo(
+                0,
+                binding.etTitle.text.toString(),
+                binding.etContents.text.toString(),
+                memo.imagePath
+            )
             finish()
         }
 
